1b11608c4abe9d7de03bc05a73c1b71653dbae0e
juliajeske <julia.jeske@wehandle.com.br>
2023-03-02 16:42:29 -0300

    Estrutura visual

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+import  logoImg  from '../assets/ignite-logo.svg'
+import { Container, Header } from '../styles/pages/app';
+import Image from 'next/image';
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+ 
+  return(
+
+    <Container>
+      <Header>
+        <Image src={logoImg} width={100} alt={''}/>
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+
+   
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,54 @@
+import camiseta from '../assets/camiseta.png'
+import { HomeContainer } from "../styles/pages/home"
+import {Product} from "../styles/pages/home"
+import Image from 'next/image';
+import { useKeenSlider} from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css';
 
 
 export default function Home() {
+
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <h1>la</h1>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta} width={520} height={480} alt={""}/>
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 70,00</span>
+        </footer>
+
+      </Product>
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta} width={520} height={480} alt={""}/>
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 70,00</span>
+        </footer>
+
+      </Product>
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta} width={520} height={480} alt={""}/>
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 70,00</span>
+        </footer>
+
+      </Product>
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta} width={520} height={480} alt={""}/>
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 70,00</span>
+        </footer>
+
+      </Product>
+    </HomeContainer>
   )
 }
